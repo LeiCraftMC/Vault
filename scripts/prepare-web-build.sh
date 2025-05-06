bb5b51a8488e80c770f5b7d617c828833bef3312
LeiCraft_ <88381277+LeiCraft@users.noreply.github.com>
2025-05-06 21:53:16 +0100

    update

--- scripts/prepare-web-build.sh
+++ scripts/prepare-web-build.sh
@@ -3,10 +3,14 @@
 TAG_NAME=$(curl -s "https://api.github.com/repos/dani-garcia/bw_web_builds/releases/latest" | grep '"tag_name":' | cut -d '"' -f 4)
 DOWNLOAD_URL="https://github.com/dani-garcia/bw_web_builds/archive/refs/tags/$TAG_NAME.tar.gz"
 
-curl -L -o "web-code-latest.tar.gz" "$DOWNLOAD_URL"
+mkdir -p ./tmp/
+
+curl -L -o "./tmp/web-code-latest.tar.gz" "$DOWNLOAD_URL"
+
+cd ./tmp/
 
 tar -xzf web-code-latest.tar.gz
 rm web-code-latest.tar.gz
 mv bw_web_builds-* web-build
 
-rsync -av web/ web-build/
+rsync -av ../web/ ./web-build/
